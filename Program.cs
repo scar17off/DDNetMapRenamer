15df2f2a65f78e9707f07cd541559634d5a28f07
scar17off <scar17off@gmail.com>
2025-02-05 14:00:54 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace MapRenamer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mapFiles = Directory.GetFiles(".", "*.map");
+
+            foreach (string mapFile in mapFiles)
+            {
+                string[] nameParts = Path.GetFileName(mapFile).Split('_');
+                if (nameParts.Length > 1)
+                {
+                    string originalName = string.Join("_", nameParts.Take(nameParts.Length - 1)) + ".map";
+                    string destPath = Path.Combine("..", "maps", originalName);
+
+                    File.Copy(mapFile, destPath, true);
+                }
+            }
+        }
+    }
+}
